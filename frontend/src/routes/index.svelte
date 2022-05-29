f0a94da602009d8b4572e590419dd9b496cd43f6
Joschka <joschka4@gmx.de>
2022-05-29 09:28:13 +0200

    fetch and display first data

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+    import { onMount } from 'svelte'
+    let users: any[] = []
+
+    onMount(async () => {
+        const res = await fetch('http://localhost:8080/users/all')
+        const data = await res.json()
+        users = data.data
+        console.log(users)
+    })
+</script>
+
+{#each users as user}
+     <h2>Name: {user.UID}</h2>
+     <p>email: {user.email}</p>
+{/each}
