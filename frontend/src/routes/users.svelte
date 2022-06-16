cc52ea340a914dc378928eff2bbd781001c60a16
Joschka <joschka4@gmx.de>
2022-06-16 19:19:29 +0200

    added basic user list

---
+++ frontend/src/routes/users.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+    import User from './../user.svelte';
+    import { onMount } from 'svelte'
+    let users: any[] = []
+
+    onMount(async () => {
+        const res = await fetch('http://localhost:8080/users/all')
+        const data = await res.json()
+        users = data.data
+        console.log(users)
+    })
+</script>
+
+{#each users as user}
+    <User UID={user.UID} email={user.email}/>
+{/each}
---
+++ frontend/src/user.svelte
@@ -0,0 +1,26 @@
+
+<script lang="ts">
+    export let UID:String;
+    export let email:String;
+</script>
+
+<div id="wrapper">
+    <h2>{UID}</h2>
+    <p>email: {email}</p>
+    <div id="buttonContainer">
+        <button>bearbeiten</button>
+        <button>löschen</button>
+    </div>
+    
+</div>
+
+<style>
+    #wrapper{
+        margin: .5em;
+        padding: .5em;
+        background-color: rgb(27, 31, 37);
+        display: inline-block;
+        width: 30em;
+        box-shadow: 5px 5px 20px rgb(10, 10, 10);
+    }
+</style>
