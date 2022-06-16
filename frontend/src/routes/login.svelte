9496dae136d8d6fd8bee2eff1615f8f5847c5ec4
Joschka <joschka4@gmx.de>
2022-06-16 19:22:55 +0200

    added basic login page

---
+++ frontend/src/routes/login.svelte
@@ -0,0 +1,34 @@
+
+<script>
+    const sendLogin = () =>{
+        console.log("Moin")
+    }
+</script>
+
+<div id="content">
+    <h1>Login</h1>
+    <p>Username</p>
+    <input type="text" name="UID" id="UID">
+    <p>Password</p>
+    <input type="password" name="password" id="password">
+
+    <div id="buttonContainer">
+        <button on:click={sendLogin}>submit</button>
+    </div>
+    
+</div>
+
+<style>
+    #content {
+        margin: 3em;
+        padding: 1em;
+        background-color: rgb(27, 31, 37);
+        display: inline-block;
+        width: 30em;
+        box-shadow: 5px 5px 20px rgb(10, 10, 10);
+    }
+
+    #buttonContainer{
+        margin: 1em;
+    }
+</style>
