a5e09c64311e4dedd98ee5222ed5f2b6182f50c9
Joschka <joschka4@gmx.de>
2022-07-24 15:14:39 +0200

    added group component to contain devices

---
+++ frontend/src/deviceGroup.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import Device from "./device.svelte";
+
+    export let groupName:String
+    export let isStored:Boolean
+    export let location:String
+    export let devices:any
+</script>
+
+<div class="container">
+    <h1>{groupName}</h1>
+    {#if isStored}
+        <h2>stored at "{location}"</h2>
+    {:else}
+        <h2>in use at "{location}"</h2>
+    {/if}
+    {#each devices as device}
+        <Device 
+            name={device.name} 
+            company={device.company} 
+            cat={device.category.name}
+            subcat={device.subcategory.name}
+            owner={device.Creator.UID}
+        />
+    {/each}
+
+    <button>add to event</button>
+    <button>add device</button>
+    <button>delete</button>
+    
+</div>
+
+<style>
+    .container{
+        width: 60%;
+        margin: 2% auto;
+        padding: .5%;
+        background-color: rgb(24, 27, 31);
+    }
+</style>
---
+++ frontend/src/deviceGroupSM.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let groupName:String
+    export let isStored:Boolean
+    export let location:String
+    export let devices:any
+</script>
+
+<div class="container">
+    <h1>{groupName}</h1>
+    {#if isStored}
+        <h2>stored at "{location}"</h2>
+    {:else}
+        <h2>in use at "{location}"</h2>
+    {/if}
+    <h2>Contains:</h2>
+    {#each devices as device}
+        <h4>{device.name}</h4>
+    {/each}
+    
+</div>
+
+<style>
+    .container{
+        width: 60%;
+        margin: 2% auto;
+        padding: .5%;
+        background-color: rgb(24, 27, 31);
+    }
+</style>
