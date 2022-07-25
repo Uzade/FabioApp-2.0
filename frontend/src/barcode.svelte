6958d585891d9d72f92eb7e2c7ae8628f7862475
Joschka <joschka4@gmx.de>
2022-07-25 17:24:56 +0200

    created simple barcode generator

---
+++ frontend/src/barcode.svelte
@@ -0,0 +1,32 @@
+
+<script lang="ts">
+    import bwipjs from 'bwip-js' 
+    import { onMount } from 'svelte'
+
+    export let id: number
+
+    onMount(() => {
+    try {
+        let canvas = bwipjs.toCanvas('canvas', {
+            bcid:        'pdf417',        // Barcode type
+            text:        '' + id,             // Text to encode
+            scale:       2,               // 3x scaling factor
+            height:      8,               // Bar height, in millimeters
+        })
+    } catch (e) {
+        // `e` may be a string or Error object
+    }
+    })
+    
+</script>
+
+<div>
+    <canvas id='canvas'></canvas>
+</div>
+
+<style>
+    canvas{
+        background-color: white;
+        padding: 2%;
+    }
+</style>
