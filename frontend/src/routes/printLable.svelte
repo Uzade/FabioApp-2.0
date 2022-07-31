b31f92db8d19664f47adce8323034ad3127fdae9
Joschka <joschka4@gmx.de>
2022-07-31 10:40:32 +0200

    added page to show lables

---
+++ frontend/src/routes/printLable.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { lableStore } from './../stores'
+    import Lable from './../lable.svelte'
+
+</script>
+
+<div id='container'>
+    <h1>Lables to print</h1>
+    {#each $lableStore as lable}
+        <Lable 
+            name={lable.groupName}
+            id={lable.id}
+            devices={lable.devices}
+        />
+    {/each}
+</div>
