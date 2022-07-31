47263bdd6e6fc0af7f583bdd7ebb5899c82a895a
Joschka <joschka4@gmx.de>
2022-07-31 10:31:30 +0200

    updated db (deleted all entries in the process)

---
+++ backend/prisma/migrations/20220731080139_added_event_name/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN "name" TEXT;
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Permition" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL DEFAULT 'unnamed-permition',
+    "seeOwnOffers" BOOLEAN NOT NULL,
+    "requestOffers" BOOLEAN NOT NULL,
+    "scanDevices" BOOLEAN NOT NULL,
+    "manageOffers" BOOLEAN NOT NULL,
+    "manageDevices" BOOLEAN NOT NULL,
+    "manageEvents" BOOLEAN NOT NULL
+);
+INSERT INTO "new_Permition" ("id", "manageDevices", "manageEvents", "manageOffers", "name", "requestOffers", "scanDevices", "seeOwnOffers") SELECT "id", "manageDevices", "manageEvents", "manageOffers", "name", "requestOffers", "scanDevices", "seeOwnOffers" FROM "Permition";
+DROP TABLE "Permition";
+ALTER TABLE "new_Permition" RENAME TO "Permition";
+CREATE UNIQUE INDEX "Permition_name_key" ON "Permition"("name");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
